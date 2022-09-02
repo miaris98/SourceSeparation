ae151df9ae1fb5ded605dccda608991a0f96d09c
dionushs <miaris54@gmail.com>
2022-09-02 17:40:52 +0300

    Added visulazation
    Added: Display of spectrograms
    Chore: Removed useless code

--- codebase/sepformer.py
+++ codebase/sepformer.py
@@ -12,17 +12,6 @@ import matplotlib.pyplot as plt
 import librosa
 import librosa.display
 
-"""
-dataset = TensorDataset(inps, tgts)
-
-loader = DataLoader(dataset, batch_size=2, collate_fn=collate_wrapper,
-                    pin_memory=True)
-
-for batch_ndx, sample in enumerate(loader):
-    print(sample.inp.is_pinned())
-    print(sample.tgt.is_pinned())
-"""
-
 def main():
     train_loader, val_loader = LibriMix.loaders_from_mini(
         task = 'sep_clean', batch_size = 4)
@@ -33,20 +22,11 @@ def main():
     #print(train_set.df.values[799][4]) # path s2
     print(train_set.df.values[0][2])
 
-    sampling_rate, samples = wavfile.read(train_set.df.values[799][2])
-    mix_tensor = torch.tensor(samples.astype(float)).float()
-
-    #mix_tensor =mix_tensor.type(torch.LongTensor)
-
     sepformer = separator.from_hparams(source="speechbrain/sepformer-wsj02mix",
                                    savedir='pretrained_models/sepformer-wsj02mix')
 
-
     ConvTasNet = BaseModel.from_pretrained("JorisCos/ConvTasNet_Libri2Mix_sepclean_8k")
     DPRNNTasNet= BaseModel.from_pretrained("mpariente/DPRNNTasNet-ks2_WHAM_sepclean")
-    #Awais = BaseModel.from_pretrained("Awais/Audio_Source_Separation")
-
-    # for custom file, change path
 
     est_sources_sep = sepformer.separate_file(train_set.df.values[0][2])
 
@@ -55,11 +35,22 @@ def main():
 
     ConvTasNet.separate(train_set.df.values[0][2], output_dir="C:\\Users\\miari\\Desktop\\repositories\\SourceSeparation\\codebase\\ConvTasNet" , force_overwrite=True)
     DPRNNTasNet.separate(train_set.df.values[0][2],output_dir="C:\\Users\\miari\\Desktop\\repositories\\SourceSeparation\\codebase\\DPRNNTasNet" ,  force_overwrite=True)
-    print("")
-   # Awais.separate("Awais-mixture.wav", force_overwrite=True)
 
+    est1 = sf.read("ConvTasNet/5400-34479-0005_4973-24515-0007_est1.wav")[0]
+    est2 = sf.read("ConvTasNet/5400-34479-0005_4973-24515-0007_est2.wav")[0]
+    fig, ax = plt.subplots(1, 2, figsize=(15, 5))
+    show_magspec(est1, sr=8000, ax=ax[0])
+    show_magspec(est2, sr=8000, ax=ax[1])
+    plt.show()
 
-
+def show_magspec(waveform, **kw):
+    return librosa.display.specshow(
+        librosa.amplitude_to_db(np.abs(librosa.stft(waveform))),
+        y_axis="log", x_axis="time",
+        **kw
+    )
 
 if __name__ == '__main__':
     main()
+
+
